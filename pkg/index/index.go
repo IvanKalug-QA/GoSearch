a5185ce7574b5c7253744f3ca1bfd3ec0d20a555
IVAN-GUY <kaluginivan2002@mail.ru>
2026-01-01 17:38:17 +0300

    refactor score and search alghorithm

--- pkg/index/index.go
+++ pkg/index/index.go
@@ -10,17 +10,24 @@ type Document struct {
 	Title string
 }
 
-func CreateIndex(d []Document) map[string]map[int]struct{} {
-	IndexDocuments := make(map[string]map[int]struct{}, 0)
+func CreateIndex(d []Document) map[string][]int {
+	has_doc := make(map[string]map[int]struct{})
+	IndexDocuments := make(map[string][]int)
 
 	for _, doc := range d {
 		splitText := strings.SplitSeq(doc.Title, " ")
 		for t := range splitText {
 			word := strings.ToLower(strings.TrimSpace(t))
-			if IndexDocuments[word] == nil {
-				IndexDocuments[word] = make(map[int]struct{})
+			if has_doc[word] == nil {
+				IndexDocuments[word] = append(IndexDocuments[word], doc.ID)
+				has_doc[word] = make(map[int]struct{})
+			} else {
+				_, ok := has_doc[word][doc.ID]
+				if !ok {
+					IndexDocuments[word] = append(IndexDocuments[word], doc.ID)
+				}
 			}
-			IndexDocuments[word][doc.ID] = struct{}{}
+			has_doc[word][doc.ID] = struct{}{}
 		}
 	}
 	return IndexDocuments
